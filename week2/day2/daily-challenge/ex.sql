f978f7bb237fa8d2d62e71581208906cda51f458
freecoss <abderrahimfarahi99@gmail.com>
2025-05-27 15:46:30 +0100

    day 2 week 2 edit

---
+++ week2/day2/daily-challenge/ex.sql
@@ -0,0 +1,39 @@
+CREATE TABLE FirstTab (
+     id INTEGER, 
+     name VARCHAR(10)
+);
+
+INSERT INTO FirstTab VALUES
+(5, 'Pawan'),
+(6, 'Sharlee'),
+(7, 'Krish'),
+(NULL, 'Avtaar');
+
+
+CREATE TABLE SecondTab (
+    id INTEGER
+);
+
+INSERT INTO SecondTab VALUES
+(5),
+(NULL);
+
+
+SELECT COUNT(*) AS Q1_Output
+FROM FirstTab AS ft 
+WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NULL );
+
+
+
+SELECT COUNT(*) AS Q2_Output
+FROM FirstTab AS ft 
+WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 );
+
+
+SELECT COUNT(*) AS Q3_Output
+FROM FirstTab AS ft 
+WHERE ft.id NOT IN ( SELECT id FROM SecondTab );
+
+SELECT COUNT(*) AS Q4_Output
+FROM FirstTab AS ft 
+WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL );

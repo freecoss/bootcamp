492504aa2173a7d5c3ad28fdbc7da50861476368
freecoss <abderrahimfarahi99@gmail.com>
2025-05-27 10:23:34 +0100

    day 4

---
+++ week2/day4/daily-challenge/ex.py
@@ -0,0 +1,21 @@
+import sqlite3
+import requests
+import random
+
+# Fetch and store 10 random countries
+conn = sqlite3.connect('countries.db')
+conn.execute('''CREATE TABLE IF NOT EXISTS countries
+             (name TEXT, capital TEXT, flag TEXT, subregion TEXT, population INTEGER)''')
+
+countries = requests.get('https://restcountries.com/v3.1/all').json()
+for country in random.sample(countries, 10):
+    conn.execute('INSERT INTO countries VALUES (?,?,?,?,?)', (
+        country['name']['common'],
+        country['capital'][0] if 'capital' in country else 'None',
+        country['flags']['png'],
+        country.get('subregion', 'None'),
+        country['population']
+    ))
+s
+conn.commit()
+conn.close()
